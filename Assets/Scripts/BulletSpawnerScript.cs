af2cd735a632dbc912ccdba9fd036b95a3eb06a8
Oskar Ciesla <oskar.ciesla01@gmail.com>
2024-03-19 09:33:02 +0100

    V0.4
    Implemented bullet firing logic (based on attack speed), implemented collision logic (with enemies and walls)

--- Assets/Scripts/BulletSpawnerScript.cs
+++ Assets/Scripts/BulletSpawnerScript.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class BulletSpawnerScript : MonoBehaviour
 {
+
+    private float attackSpeed = 0.5f;
+    private float nextTimeToFire = 0f;
+
+    public GameObject bulletPrefab;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Time.time >= nextTimeToFire)
+        {
+            nextTimeToFire = Time.time + 1f / attackSpeed;
+            FireBullet();
+        } 
+    }
+
+    void FireBullet()
+    {
+        Instantiate(bulletPrefab, transform.position, transform.rotation);
     }
 }
